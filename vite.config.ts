b8413858907bea11df0dddf6b2dbe468708b8bc1
tromxx <husik927@gmail.com>
2025-12-31 11:04:11 +0900

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,51 @@
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+// import path from "path";
+// import runtimeErrorOverlay from "@replit/vite-plugin-runtime-error-modal";
+
+// export default defineConfig({
+//   plugins: [
+//     react(),
+//     runtimeErrorOverlay(),
+//     ...(process.env.NODE_ENV !== "production" &&
+//     process.env.REPL_ID !== undefined
+//       ? [
+//           await import("@replit/vite-plugin-cartographer").then((m) =>
+//             m.cartographer(),
+//           ),
+//           await import("@replit/vite-plugin-dev-banner").then((m) =>
+//             m.devBanner(),
+//           ),
+//         ]
+//       : []),
+//   ],
+//   resolve: {
+//     alias: {
+//       "@": path.resolve(import.meta.dirname, "client", "src"),
+//       "@shared": path.resolve(import.meta.dirname, "shared"),
+//       "@assets": path.resolve(import.meta.dirname, "attached_assets"),
+//     },
+//   },
+//   root: path.resolve(import.meta.dirname, "client"),
+//   build: {
+//     outDir: path.resolve(import.meta.dirname, "dist/public"),
+//     emptyOutDir: true,
+//   },
+//   server: {
+//     fs: {
+//       strict: true,
+//       deny: ["**/.*"],
+//     },
+//   },
+// });
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 import runtimeErrorOverlay from "@replit/vite-plugin-runtime-error-modal";
 
 export default defineConfig({
+  base: "/Wizley/",
+
   plugins: [
     react(),
     runtimeErrorOverlay(),
@@ -19,6 +61,7 @@ export default defineConfig({
         ]
       : []),
   ],
+
   resolve: {
     alias: {
       "@": path.resolve(import.meta.dirname, "client", "src"),
@@ -26,15 +69,11 @@ export default defineConfig({
       "@assets": path.resolve(import.meta.dirname, "attached_assets"),
     },
   },
+
   root: path.resolve(import.meta.dirname, "client"),
+
   build: {
-    outDir: path.resolve(import.meta.dirname, "dist/public"),
+    outDir: path.resolve(import.meta.dirname, "docs"),
     emptyOutDir: true,
-  },
-  server: {
-    fs: {
-      strict: true,
-      deny: ["**/.*"],
-    },
   },
 });
